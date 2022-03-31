35140799daf64eb0a2f863034e954b1f8e698790
Михаил Воронков <redvoronik@yandex.ru>
2022-03-31 13:49:22 +0300

    Добавил фильтрацию имён проектов и имён задач

--- templates/main.php
+++ templates/main.php
@@ -5,7 +5,7 @@
                     <ul class="main-navigation__list">
                         <?php foreach($projects as $project_name) :?>
                             <li class="main-navigation__list-item">
-                                <a class="main-navigation__list-item-link" href="#"><?=$project_name;?></a>
+                                <a class="main-navigation__list-item-link" href="#"><?=strip_tags($project_name);?></a>
                                 <span class="main-navigation__list-item-count"><?= tasks_count($tasks, $project_name); ?></span>
                             </li>
                         <?php endforeach; ?>
@@ -47,7 +47,7 @@
                             <td class="task__select">
                                 <label class="checkbox task__checkbox">
                                     <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" value="1">
-                                    <span class="checkbox__text"><?=$task['name'];?></span>
+                                    <span class="checkbox__text"><?=strip_tags($task['name']);?></span>
                                 </label>
                             </td>
 
