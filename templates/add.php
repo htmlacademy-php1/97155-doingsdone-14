abcdde9ebcf21b95a289faf2b155405198ac4b30
Михаил Воронков <redvoronik@gmail.com>
2022-04-25 21:26:29 +0300

    Добавил вывод проектов из БД в селект

--- templates/add.php
+++ templates/add.php
@@ -30,7 +30,9 @@
                 <label class="form__label" for="project">Проект <sup>*</sup></label>
 
                 <select class="form__input form__input--select" name="project" id="project">
-                <option value="">Входящие</option>
+                    <?php foreach ($projects as $project): ?>
+                        <option value="<?= $project['id']; ?>"><?= $project['name']; ?></option>
+                    <?php endforeach; ?>
                 </select>
             </div>
 
