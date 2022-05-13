75505ffb05c342a37186f0fdbcebc1a0f7de0dcd
Михаил Воронков <redvoronik@yandex.ru>
2022-05-13 14:59:05 +0300

    Добавил вывод ошибокв шаблон авторизации

--- templates/auth.php
+++ templates/auth.php
@@ -1,25 +1,30 @@
       <section class="content__side">
         <p class="content__side-info">Если у вас уже есть аккаунт, авторизуйтесь на сайте</p>
 
-        <a class="button button--transparent content__side-button" href="form-authorization.html">Войти</a>
+        <a class="button button--transparent content__side-button" href="/auth.php">Войти</a>
       </section>
 
       <main class="content__main">
         <h2 class="content__main-heading">Вход на сайт</h2>
 
-        <form class="form" action="index.html" method="post" autocomplete="off">
+        <form class="form" action="" method="post" autocomplete="off">
           <div class="form__row">
             <label class="form__label" for="email">E-mail <sup>*</sup></label>
 
-            <input class="form__input form__input--error" type="text" name="email" id="email" value="" placeholder="Введите e-mail">
+            <input class="form__input <?= (isset($errors['email'])) ? 'form__input--error' : '' ?>" type="text" name="email" id="email" value="<?= (isset($errors)) ? get_post_val('email') : '' ?>" placeholder="Введите e-mail">
 
-            <p class="form__message">E-mail введён некорректно</p>
+            <?php if (isset($errors['email'])): ?>
+                <p class="form__message"><?= $errors['email'] ?></p>
+            <?php endif; ?>
           </div>
 
           <div class="form__row">
             <label class="form__label" for="password">Пароль <sup>*</sup></label>
 
-            <input class="form__input" type="password" name="password" id="password" value="" placeholder="Введите пароль">
+            <input class="form__input <?= (isset($errors['password'])) ? 'form__input--error' : '' ?>"" type="password" name="password" id="password" value="" placeholder="Введите пароль">
+            <?php if (isset($errors['password'])): ?>
+                <p class="form__message"><?= $errors['password'] ?></p>
+            <?php endif; ?>
           </div>
 
           <div class="form__row form__row--controls">
